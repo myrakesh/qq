24d72e6cf8e7863b7eebc64e43e9b16292c145af
your.user.name <in.rakesh003@gmaail.com>
2020-12-13 11:30:50 +0530

    first commit

---
+++ question.js
@@ -0,0 +1,303 @@
+let questions = [
+  {
+    id: 1,
+    question: "A beam of unpolarized light of intensity \\(I_o\\) passes through a combination of an ideal polarizer and an idea analyzer with their transmission axes at \\(60^o\\) . What is the intensity of the beam coming out at the other end?",
+    queimg:"image/a.png",
+    answer: "2",
+    exp:"When an unploarized of intensity \\(I_o\\) passes through a polarizer then intensity becomes half i.e I'=\\(\\frac{I_o}{2}\\) .<p> Now from Maulus law I=I\\(cos^2θ\\) where θ=\\(60^o\\)</p> <p> ⇒  I=\\(\\frac{I_o}{2}\\)\\(cos^2\\)\\( 60^o\\) = \\(\\frac{I_o}{2}\\)x\\((\\frac{1}{2})^2\\)=\\(\\frac{I_o}{8}\\)</p>",
+    img:"image/a.png",
+    options: [
+      "$$\\begin{vmatrix}a & b \\\\ c & d \\end{vmatrix}$$",
+      "\\(\\frac{I_o}{2}\\)",
+      "\\(\\frac{I_o}{4}\\)",
+      "\\(\\frac{I_o}{8}\\)"
+    ],
+  },
+    {
+      id: 2,
+      question: "",
+      answer: "",
+      queimg:"",
+      exp:"",
+      img:"#",
+      math:"",
+      options: [
+        "",
+        "",
+        "",
+        ""
+      ]
+    },
+    {
+      id: 3,
+      question: "",
+      answer: "",
+      queimg:"",
+      exp:"",
+      img:"#",
+      math:"",
+      options: [
+        "",
+        "",
+        "",
+        ""
+      ]
+    },
+    {
+      id: 4,
+      question: "",
+      answer: "",
+      queimg:"",
+      exp:"",
+      img:"#",
+      math:"",
+      options: [
+        "",
+        "",
+        "",
+        ""
+      ]
+    },
+    {
+      id: 5,
+      question: "",
+      answer: "",
+      queimg:"",
+      exp:"",
+      img:"#",
+      math:"",
+      options: [
+        "",
+        "",
+        "",
+        ""
+      ]
+    },
+    {
+      id: 6,
+      question: "",
+      answer: "",
+      queimg:"",
+      exp:"",
+      img:"#",
+      math:"",
+      options: [
+        "",
+        "",
+        "",
+        ""
+      ]
+    },
+    {
+      id: 7,
+      question: "",
+      answer: "",
+      queimg:"",
+      exp:"",
+      img:"#",
+      math:"",
+      options: [
+        "",
+        "",
+        "",
+        ""
+      ]
+    },
+    {
+      id: 8,
+      question: "",
+      answer: "",
+      queimg:"",
+      exp:"",
+      img:"#",
+      math:"",
+      options: [
+        "",
+        "",
+        "",
+        ""
+      ]
+    },
+    {
+      id: 9,
+      question: "",
+      answer: "",
+      queimg:"",
+      exp:"",
+      img:"#",
+      math:"",
+      options: [
+        "",
+        "",
+        "",
+        ""
+      ]
+    },
+    {
+      id: 10,
+      question: "",
+      answer: "",
+      queimg:"",
+      exp:"",
+      img:"#",
+      math:"",
+      options: [
+        "",
+        "",
+        "",
+        ""
+      ]
+    },
+    {
+      id: 11,
+      question: "",
+      answer: "",
+      queimg:"",
+      exp:"",
+      img:"#",
+      math:"",
+      options: [
+        "",
+        "",
+        "",
+        ""
+      ]
+    },
+    {
+      id: 12,
+      question: "",
+      answer: "",
+      queimg:"",
+      exp:"",
+      img:"#",
+      math:"",
+      options: [
+        "",
+        "",
+        "",
+        ""
+      ]
+    },
+    {
+      id: 13,
+      question: "",
+      answer: "",
+      queimg:"",
+      exp:"",
+      img:"#",
+      math:"",
+      options: [
+        "",
+        "",
+        "",
+        ""
+      ]
+    },
+    {
+      id: 14,
+      question: "",
+      answer: "",
+      queimg:"",
+      exp:"",
+      img:"#",
+      math:"",
+      options: [
+        "",
+        "",
+        "",
+        ""
+      ]
+    },
+    {
+      id: 15,
+      question: "",
+      answer: "",
+      queimg:"",
+      exp:"",
+      img:"#",
+      math:"",
+      options: [
+        "",
+        "",
+        "",
+        ""
+      ]
+    },
+    {
+      id: 16,
+      question: "",
+      answer: "",
+      queimg:"",
+      exp:"",
+      img:"#",
+      math:"",
+      options: [
+        "",
+        "",
+        "",
+        ""
+      ]
+    },
+    {
+      id: 17,
+      question: "",
+      answer: "",
+      queimg:"",
+      exp:"",
+      img:"#",
+      math:"",
+      options: [
+        "",
+        "",
+        "",
+        ""
+      ]
+    },
+    {
+      id: 18,
+      question: "",
+      answer: "",
+      queimg:"",
+      exp:"",
+      img:"#",
+      math:"",
+      options: [
+        "",
+        "",
+        "",
+        ""
+      ]
+    },
+    {
+      id: 19,
+      question: "",
+      answer: "",
+      queimg:"",
+      exp:"",
+      img:"#",
+      math:"",
+      options: [
+        "",
+        "",
+        "",
+        ""
+      ]
+    },
+    {
+      id: 20,
+      question: "",
+      answer: "",
+      queimg:"",
+      exp:"",
+      img:"#",
+      math:"",
+      options: [
+        "",
+        "",
+        "",
+        ""
+      ]
+    },
+  
+  ];
+  
